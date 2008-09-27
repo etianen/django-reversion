fe6e790cfcfb8b7d0d29ab928da0fbe4351f017d
david@etianen.com <david@etianen.com@ab604574-8a94-11dd-aa62-cdabd1f4ca1e>
2008-09-27 16:04:36 +0000

    Removed dependency on frozenset
    git-svn-id: https://django-reversion.googlecode.com/svn/trunk@41 ab604574-8a94-11dd-aa62-cdabd1f4ca1e

--- src/reversion/settings.py
+++ src/reversion/settings.py
@@ -7,9 +7,9 @@ from django.db import settings
 # A tuple of models that should be placed under version control.  Each model
 # should be in the form app_label.ModelName.
 try:
-    VERSION_CONTROLLED_MODELS = frozenset(settings.VERSION_CONTROLLED_MODELS)
+    VERSION_CONTROLLED_MODELS = settings.VERSION_CONTROLLED_MODELS
 except AttributeError:
-    VERSION_CONTROLLED_MODELS = frozenset()
+    VERSION_CONTROLLED_MODELS = ()
     
 
 # The file storage implementation used by the version file storage mechanism.
