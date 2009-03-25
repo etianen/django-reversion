21cfd3733c60d3083a61c314b543ab6ed073cec4
david@etianen.com <david@etianen.com@ab604574-8a94-11dd-aa62-cdabd1f4ca1e>
2009-03-25 11:13:55 +0000

    git-svn-id: https://django-reversion.googlecode.com/svn/trunk@159 ab604574-8a94-11dd-aa62-cdabd1f4ca1e

--- src/setup.py
+++ src/setup.py
@@ -3,23 +3,23 @@ from distutils.command.install import INSTALL_SCHEMES
 
 
 for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
+    scheme["data"] = scheme["purelib"]
 
 
 setup(name="django-reversion",
-      version="1.1",
+      version="1.2",
       description="An extension to the Django web framework that provides comprehensive version control facilities",
       author="David Hall",
       author_email="david@etianen.com",
       url="http://code.google.com/p/django-reversion/",
-      download_url="http://django-reversion.googlecode.com/files/django-reversion-1.1.tar.gz",
+      download_url="http://django-reversion.googlecode.com/files/django-reversion-1.2.tar.gz",
       packages=["reversion",],
       package_dir={"reversion": "reversion"},
-      data_files=[['reversion/templates/reversion', ['reversion/templates/reversion/object_history.html',
-                                                     'reversion/templates/reversion/change_list.html',
-                                                     'reversion/templates/reversion/recover_form.html',
-                                                     'reversion/templates/reversion/revision_form.html',
-                                                     'reversion/templates/reversion/recover_list.html']]],
+      data_files=[["reversion/templates/reversion", ["reversion/templates/reversion/object_history.html",
+                                                     "reversion/templates/reversion/change_list.html",
+                                                     "reversion/templates/reversion/recover_form.html",
+                                                     "reversion/templates/reversion/revision_form.html",
+                                                     "reversion/templates/reversion/recover_list.html"]]],
       classifiers=["Development Status :: 5 - Production/Stable",
                    "Environment :: Web Environment",
                    "Intended Audience :: Developers",
