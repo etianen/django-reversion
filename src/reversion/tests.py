27279bff45febb0fadabeb25abb0a4e2df2c796b
dave@pod.me.uk <dave@pod.me.uk@ab604574-8a94-11dd-aa62-cdabd1f4ca1e>
2009-08-30 20:26:22 +0000

    Doctests no longer require diff_match_patch.
    git-svn-id: https://django-reversion.googlecode.com/svn/trunk@212 ab604574-8a94-11dd-aa62-cdabd1f4ca1e

--- src/reversion/tests.py
+++ src/reversion/tests.py
@@ -86,15 +86,6 @@ View unique versions of an object:
     >>> Version.objects.get_unique_for_object(versioned_site)
     [<Version: www.site-rev-1.com>, <Version: www.site-rev-2.com>, <Version: www.site-rev-1.com>]
     
-Generate diffs for two different versions:
-
-    >>> version_0 = Version.objects.get_for_object(versioned_site)[0]
-    >>> version_1 = Version.objects.get_for_object(versioned_site)[1]
-    >>> generate_patch(version_0, version_1, "domain")
-    '@@ -10,9 +10,9 @@\\n rev-\\n-1\\n+2\\n .com\\n'
-    >>> generate_patch_html(version_0, version_1, "domain")
-    u'<SPAN TITLE="i=0">www.site-rev-</SPAN><DEL STYLE="background:#FFE6E6;" TITLE="i=13">1</DEL><INS STYLE="background:#E6FFE6;" TITLE="i=13">2</INS><SPAN TITLE="i=14">.com</SPAN>'
-    
 Delete a version controlled object:
 
     >>> versioned_site.delete()
@@ -260,7 +251,7 @@ All done!
 
 from __future__ import with_statement
 
-import datetime
+import datetime, unittest
 
 from django.contrib import admin
 from django.contrib.admin.models import LogEntry, DELETION
@@ -271,6 +262,43 @@ from django.db import models
 
 import reversion
 from reversion.admin import VersionAdmin
-from reversion.helpers import patch_admin, generate_patch, generate_patch_html
+from reversion.helpers import patch_admin
 from reversion.models import Version, Revision
 
+
+try:
+    from reversion.helpers import generate_patch, generate_patch_html
+except ImportError:
+    pass
+else:
+    
+    class PatchTest(unittest.TestCase):
+        
+        """Tests the patch generation functionality."""
+        
+        def setUp(self):
+            """Sets up a versioned site model to test."""
+            reversion.register(Site)
+            with reversion.revision:
+                site = Site.objects.create(name="site", domain="www.site-rev-1.com")
+            with reversion.revision:
+                site.domain = "www.site-rev-2.com"
+                site.save()
+            self.site = site
+        
+        def testCanGeneratePatch(self):
+            """Tests that text and HTML patches can be generated."""
+            version_0 = Version.objects.get_for_object(self.site)[0]
+            version_1 = Version.objects.get_for_object(self.site)[1]
+            self.assertEqual(generate_patch(version_0, version_1, "domain"),
+                             "@@ -10,9 +10,9 @@\n rev-\n-1\n+2\n .com\n")
+            self.assertEqual(generate_patch_html(version_0, version_1, "domain"),
+                             u'<SPAN TITLE="i=0">www.site-rev-</SPAN><DEL STYLE="background:#FFE6E6;" TITLE="i=13">1</DEL><INS STYLE="background:#E6FFE6;" TITLE="i=13">2</INS><SPAN TITLE="i=14">.com</SPAN>')
+        
+        def tearDown(self):
+            """Deletes the versioned site model."""
+            reversion.unregister(Site)
+            self.site.delete()
+            Version.objects.all().delete()
+            
+            
