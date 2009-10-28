01f773c2b283b0882d65b6b3b4b93d8bbde20e36
dave@pod.me.uk <dave@pod.me.uk@ab604574-8a94-11dd-aa62-cdabd1f4ca1e>
2009-10-28 14:56:13 +0000

    Added locale files to pypy package.
    git-svn-id: https://django-reversion.googlecode.com/svn/trunk@246 ab604574-8a94-11dd-aa62-cdabd1f4ca1e

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,14 @@
+from glob import glob
 from distutils.core import setup
 from distutils.command.install import INSTALL_SCHEMES
 
 
 for scheme in INSTALL_SCHEMES.values():
     scheme["data"] = scheme["purelib"]
-
-
+    
+    
 setup(name="django-reversion",
-      version="1.2",
+      version="1.2.1",
       description="An extension to the Django web framework that provides comprehensive version control facilities",
       author="David Hall",
       author_email="david@etianen.com",
@@ -15,11 +16,7 @@ setup(name="django-reversion",
       download_url="http://django-reversion.googlecode.com/files/django-reversion-1.2.tar.gz",
       packages=["reversion",],
       package_dir={"reversion": "reversion"},
-      data_files=[["reversion/templates/reversion", ["reversion/templates/reversion/object_history.html",
-                                                     "reversion/templates/reversion/change_list.html",
-                                                     "reversion/templates/reversion/recover_form.html",
-                                                     "reversion/templates/reversion/revision_form.html",
-                                                     "reversion/templates/reversion/recover_list.html"]]],
+      data_files=[["reversion/templates/reversion", glob("reversion/templates/reversion/*.html") + glob("reversion/locale/*/LC_MESSAGES/django.*")]],
       classifiers=["Development Status :: 5 - Production/Stable",
                    "Environment :: Web Environment",
                    "Intended Audience :: Developers",
