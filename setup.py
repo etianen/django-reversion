095320d551c3c6be16a93055855da22455c2d17e
david.etianen <david.etianen@ab604574-8a94-11dd-aa62-cdabd1f4ca1e>
2010-05-31 17:27:32 +0000

    Moved metadata to project root
    git-svn-id: https://django-reversion.googlecode.com/svn/trunk@281 ab604574-8a94-11dd-aa62-cdabd1f4ca1e

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
 from glob import glob
 from distutils.core import setup
-from distutils.command.install import INSTALL_SCHEMES
 
 
-for scheme in INSTALL_SCHEMES.values():
-    scheme["data"] = scheme["purelib"]
-    
-    
 setup(name="django-reversion",
       version="1.3",
       description="An extension to the Django web framework that provides comprehensive version control facilities",
@@ -14,10 +9,10 @@ setup(name="django-reversion",
       author_email="dave@etianen.com",
       url="http://code.google.com/p/django-reversion/",
       download_url="http://django-reversion.googlecode.com/files/django-reversion-1.3.tar.gz",
-      packages=["reversion", "reversion.templatetags", "reversion.management"],
-      package_dir={"reversion": "reversion"},
-      data_files=[("", glob("reversion/templates/reversion/*.html")),
-                  ("", glob("reversion/locale/*/LC_MESSAGES/django.*"))],
+      zip_safe=False,
+      packages=["reversion", "reversion.management", "reversion.templatetags"],
+      package_dir={"reversion": "src/reversion"},
+      package_data = {"reversion": ["locale/*/LC_MESSAGES/django.*", "templates/reversion/*.html"]},
       classifiers=["Development Status :: 5 - Production/Stable",
                    "Environment :: Web Environment",
                    "Intended Audience :: Developers",
