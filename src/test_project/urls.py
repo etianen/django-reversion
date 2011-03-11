e66dfd0eb58410bcb8a97f033e910723cc4f711e
Dave Hall <david@etianen.com>
2011-03-11 16:19:25 +0000

    Minor foobar in the new settings.

--- src/test_project/urls.py
+++ src/test_project/urls.py
@@ -6,10 +6,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'test_project.views.home', name='home'),
-    url(r'^test_project/', include('test_project.foo.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
