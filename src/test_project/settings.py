7488615ed718c2118134a71215a44bccbbbe6431
Dave Hall <david@etianen.com>
2011-03-11 16:11:51 +0000

    Added in migration for schema change

--- src/test_project/settings.py
+++ src/test_project/settings.py
@@ -124,6 +124,8 @@ INSTALLED_APPS = (
     'south',
 )
 
+SOUTH_TESTS_MIGRATE = False
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
