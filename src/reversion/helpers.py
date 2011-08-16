b29c2870e4f1a6aaf5a3973315cc777be687172d
Dave Hall <dave@etianen.com>
2011-08-16 17:54:42 +0100

    Renaming style param to cleanup for generating diffs.

--- src/reversion/helpers.py
+++ src/reversion/helpers.py
@@ -41,38 +41,42 @@ except ImportError:
 else:
     dmp = diff_match_patch()
 
-    def generate_diffs(old_version, new_version, field_name, style):
+    def generate_diffs(old_version, new_version, field_name, cleanup):
         """Generates a diff array for the named field between the two versions."""
         # Extract the text from the versions.
         old_text = old_version.field_dict[field_name] or u""
         new_text = new_version.field_dict[field_name] or u""
         # Generate the patch.
         diffs = dmp.diff_main(str(old_text), str(new_text))
-        if style == "semantic":
+        if cleanup == "semantic":
             dmp.diff_cleanupSemantic(diffs)
-        if style == "efficiency":
+        elif cleanup == "efficiency":
             dmp.diff_cleanupEfficiency(diffs)
+        elif cleanup is None:
+            pass
+        else:
+            raise ValueError("cleanup parameter should be one of 'semantic', 'efficiency' or None.")
         return diffs
     
-    def generate_patch(old_version, new_version, field_name, style=None):
+    def generate_patch(old_version, new_version, field_name, cleanup=None):
         """
         Generates a text patch of the named field between the two versions.
         
-        The style parameter can be None, "semantic" or "efficiency" to clean up the diff
+        The cleanup parameter can be None, "semantic" or "efficiency" to clean up the diff
         for greater human readibility.
         """
-        diffs = generate_diffs(old_version, new_version, field_name, style)
+        diffs = generate_diffs(old_version, new_version, field_name, cleanup)
         patch = dmp.patch_make(diffs)
         return dmp.patch_toText(patch)
     
-    def generate_patch_html(old_version, new_version, field_name, style=None):
+    def generate_patch_html(old_version, new_version, field_name, cleanup=None):
         """
         Generates a pretty html version of the differences between the named 
         field in two versions.
         
-        The style parameter can be None, "semantic" or "efficiency" to clean up the diff
+        The cleanup parameter can be None, "semantic" or "efficiency" to clean up the diff
         for greater human readibility.
         """
-        diffs = generate_diffs(old_version, new_version, field_name, style)
+        diffs = generate_diffs(old_version, new_version, field_name, cleanup)
         return dmp.diff_prettyHtml(diffs)
     
