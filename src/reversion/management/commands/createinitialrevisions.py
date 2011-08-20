ab4dde34633670dad79f45eca01dd2ba4dd1880d
Dave Hall <dave@etianen.com>
2011-08-20 15:05:01 +0100

    Added in speedup for createinitialrevisions.

--- src/reversion/management/commands/createinitialrevisions.py
+++ src/reversion/management/commands/createinitialrevisions.py
@@ -4,13 +4,14 @@ from django import VERSION
 from django.core.exceptions import ImproperlyConfigured
 from django.core.management.base import BaseCommand
 from django.core.management.base import CommandError
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.importlib import import_module
 from django.utils.datastructures import SortedDict
 from django.utils.encoding import smart_unicode
 
 from reversion import revision
-from reversion.models import Version
+from reversion.models import Version, has_int_pk
 
 
 class Command(BaseCommand):
@@ -86,19 +87,33 @@ class Command(BaseCommand):
         # Check all models for empty revisions.
         if revision.is_registered(model_class):
             created_count = 0
-            # HACK: This join can't be done in the database, due to incompatibilities
-            # between unicode object_ids and integer pks on strict backends like postgres.
-            for obj in model_class._default_manager.iterator():
-                if Version.objects.get_for_object(obj).count() == 0:
-                    try:
-                        self.version_save(obj, comment)
-                    except:
-                        print "ERROR: Could not save initial version for %s %s." % (model_class.__name__, obj.pk)
-                        raise
-                    created_count += 1
+            content_type = ContentType.objects.get_for_model(model_class)
+            versioned_pk_queryset = Version.objects.filter(content_type=content_type).all()
+            live_objs = model_class._default_manager.all()
+            if has_int_pk(model_class):
+                # We can do this as a fast database join!
+                live_objs = live_objs.exclude(
+                    pk__in = versioned_pk_queryset.values_list("object_id_int", flat=True)
+                ).iterator()
+            else:
+                # HACK: This join can't be done in the database, due to incompatibilities
+                # between unicode object_ids and integer pks on strict backends like postgres.
+                live_objs = (
+                    obj
+                    for obj in live_objs.iterator()
+                    if not Version.objects.get_for_object(obj).exists()
+                )
+            # Save all the versions.
+            for obj in live_objs:
+                try:
+                    self.version_save(obj, comment)
+                except:
+                    print "ERROR: Could not save initial version for %s %s." % (model_class.__name__, obj.pk)
+                    raise
+                created_count += 1
             # Print out a message, if feeling verbose.
-            if created_count > 0 and verbosity >= 2:
-                print u"Created %s initial revisions for model %s." % (created_count, smart_unicode(model_class._meta.verbose_name))
+            if verbosity >= 2:
+                print u"Created %s initial revision(s) for model %s." % (created_count, smart_unicode(model_class._meta.verbose_name))
         else:
             if verbosity >= 2:
                 print u"Model %s is not registered."  % (smart_unicode(model_class._meta.verbose_name))
