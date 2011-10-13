985f7aa7830f9a29c909bdbc85e958b896e09f83
Dave Hall <dave@etianen.com>
2011-10-13 11:12:44 +0100

    Enabling one-to-one fields to be used with the indexed speedups

--- src/reversion/models.py
+++ src/reversion/models.py
@@ -109,9 +109,14 @@ VERSION_TYPE_CHOICES = (
 
 def has_int_pk(model):
     """Tests whether the given model has an integer primary key."""
+    pk = model._meta.pk
     return (
-        isinstance(model._meta.pk, (models.IntegerField, models.AutoField)) and
-        not isinstance(model._meta.pk, models.BigIntegerField)
+        (
+            isinstance(pk, (models.IntegerField, models.AutoField)) and
+            not isinstance(pk, models.BigIntegerField)
+        ) or (
+            isinstance(pk, models.ForeignKey) and has_int_pk(pk.rel.to)
+        )
     )
 
 
