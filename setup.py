5e0f7ba6ed8468ce590d4f1f9bf49efcebcc2100
Dave Hall <dave@etianen.com>
2013-02-27 11:52:56 +0000

    Advertising support for python3.

--- setup.py
+++ setup.py
@@ -33,4 +33,8 @@ setup(name="django-reversion",
                    "License :: OSI Approved :: BSD License",
                    "Operating System :: OS Independent",
                    "Programming Language :: Python",
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
                    "Framework :: Django",])
