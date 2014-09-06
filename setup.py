2864da39d6b1bc85d0d795e8cf06206d23de461a
Dave Hall <dave@etianen.com>
2014-09-06 15:36:26 +0100

    Advertising Python 3.4 support

--- setup.py
+++ setup.py
@@ -36,4 +36,5 @@ setup(name="django-reversion",
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3.2',
                    'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
                    "Framework :: Django",])
