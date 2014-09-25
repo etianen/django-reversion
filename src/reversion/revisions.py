6b6913b566b0b5710597314d1311bfddcf89730b
Erwin Junge <erwin@junge.nl>
2014-09-25 10:10:01 +0200

    Also follow relationships immediately on pre_delete

--- src/reversion/revisions.py
+++ src/reversion/revisions.py
@@ -604,9 +604,14 @@ class RevisionManager(object):
                 # be modified by django right after this.
                 # don't use a lambda, but get the data out now.
                 version_data = adapter.get_version_data(instance, self._revision_context_manager._db)
+                self._revision_context_manager.add_to_context(self, instance, version_data)
+                for obj in self._follow_relationships([instance]):
+                    adapter = self.get_adapter(obj.__class__)
+                    version_data = adapter.get_version_data(obj, self._revision_context_manager._db)
+                    self._revision_context_manager.add_to_context(self, obj, version_data)
             else:
                 version_data = lambda: adapter.get_version_data(instance, self._revision_context_manager._db)
-            self._revision_context_manager.add_to_context(self, instance, version_data)
+                self._revision_context_manager.add_to_context(self, instance, version_data)
 
 
 # A shared revision manager.
