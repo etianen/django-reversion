2792b4ddbd0305ab6ba8ead371447cd0ab0eb815
Dave Hall <dave@etianen.com>
2015-07-15 19:26:45 +0100

    Fixing migrations for Django 1.7

--- src/tests/test_reversion/migrations/0001_initial.py
+++ src/tests/test_reversion/migrations/0001_initial.py
@@ -9,137 +9,158 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('reversion', '0002_auto_20141216_1509'),
-        ('contenttypes', '0002_remove_content_type_name'),
+        ('contenttypes', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='InlineTestChildGenericModel',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('object_id', models.IntegerField(db_index=True)),
                 ('name', models.CharField(max_length=100)),
                 ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
             ],
+            options={
+            },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='InlineTestChildModel',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('name', models.CharField(max_length=100)),
             ],
+            options={
+            },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='InlineTestParentModel',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('name', models.CharField(max_length=100)),
             ],
+            options={
+            },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='InlineTestUnrelatedChildModel',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
             ],
+            options={
+            },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='InlineTestUnrelatedParentModel',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
             ],
+            options={
+            },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='ParentTestAdminModel',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('parent_name', models.CharField(max_length=200)),
             ],
-        ),
-        migrations.CreateModel(
-            name='ReversionTestModel1',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=100)),
-            ],
             options={
-                'abstract': False,
             },
-        ),
-        migrations.CreateModel(
-            name='ReversionTestModel2',
-            fields=[
-                ('name', models.CharField(max_length=100)),
-                ('id', models.CharField(serialize=False, max_length=100, default=test_reversion.models.get_str_pk, primary_key=True)),
-            ],
-            options={
-                'abstract': False,
-            },
-        ),
-        migrations.CreateModel(
-            name='ReversionTestModel3',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=100)),
-            ],
-            options={
-                'abstract': False,
-            },
-        ),
-        migrations.CreateModel(
-            name='RevisionMeta',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('age', models.IntegerField()),
-                ('revision', models.OneToOneField(to='reversion.Revision')),
-            ],
-        ),
-        migrations.CreateModel(
-            name='TestFollowModel',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=100)),
-            ],
-            options={
-                'abstract': False,
-            },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='ChildTestAdminModel',
             fields=[
-                ('parenttestadminmodel_ptr', models.OneToOneField(primary_key=True, serialize=False, auto_created=True, to='test_reversion.ParentTestAdminModel', parent_link=True)),
+                ('parenttestadminmodel_ptr', models.OneToOneField(parent_link=True, to='test_reversion.ParentTestAdminModel', auto_created=True, serialize=False, primary_key=True)),
                 ('child_name', models.CharField(max_length=200)),
             ],
+            options={
+            },
             bases=('test_reversion.parenttestadminmodel',),
+        ),
+        migrations.CreateModel(
+            name='ReversionTestModel1',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='ReversionTestModel1Child',
             fields=[
-                ('reversiontestmodel1_ptr', models.OneToOneField(primary_key=True, serialize=False, auto_created=True, to='test_reversion.ReversionTestModel1', parent_link=True)),
+                ('reversiontestmodel1_ptr', models.OneToOneField(parent_link=True, to='test_reversion.ReversionTestModel1', auto_created=True, serialize=False, primary_key=True)),
             ],
             options={
                 'abstract': False,
             },
             bases=('test_reversion.reversiontestmodel1',),
         ),
-        migrations.AddField(
-            model_name='testfollowmodel',
-            name='test_model_1',
-            field=models.ForeignKey(to='test_reversion.ReversionTestModel1'),
+        migrations.CreateModel(
+            name='ReversionTestModel2',
+            fields=[
+                ('name', models.CharField(max_length=100)),
+                ('id', models.CharField(serialize=False, primary_key=True, default=test_reversion.models.get_str_pk, max_length=100)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
         ),
-        migrations.AddField(
-            model_name='testfollowmodel',
-            name='test_model_2s',
-            field=models.ManyToManyField(to='test_reversion.ReversionTestModel2'),
+        migrations.CreateModel(
+            name='ReversionTestModel3',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='RevisionMeta',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('age', models.IntegerField()),
+                ('revision', models.OneToOneField(to='reversion.Revision')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='TestFollowModel',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('test_model_1', models.ForeignKey(to='test_reversion.ReversionTestModel1')),
+                ('test_model_2s', models.ManyToManyField(to='test_reversion.ReversionTestModel2')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
         ),
         migrations.AddField(
             model_name='inlinetestunrelatedchildmodel',
             name='parent',
             field=models.ForeignKey(to='test_reversion.InlineTestUnrelatedParentModel'),
+            preserve_default=True,
         ),
         migrations.AddField(
             model_name='inlinetestchildmodel',
             name='parent',
             field=models.ForeignKey(to='test_reversion.InlineTestParentModel', related_name='children'),
+            preserve_default=True,
         ),
         migrations.CreateModel(
             name='ReversionTestModel1Proxy',
