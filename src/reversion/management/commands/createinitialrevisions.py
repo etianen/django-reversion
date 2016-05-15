50959a17f8f6bb79c599a3ef8c51c63b3a31a8b9
Michael Shannon <mshannon@urbn.com>
2016-05-14 21:18:51 -0400

    Change Object_Id to Char and Index
    - Remove object_id_int
    - Combination index for content_type and object_id for performance

--- src/reversion/management/commands/createinitialrevisions.py
+++ src/reversion/management/commands/createinitialrevisions.py
@@ -14,7 +14,7 @@ from django.utils import translation
 from django.utils.encoding import force_text
 
 from reversion.revisions import default_revision_manager
-from reversion.models import Version, has_int_pk
+from reversion.models import Version
 
 
 def get_app(app_label):
@@ -121,16 +121,12 @@ class Command(BaseCommand):
             content_type = ContentType.objects.db_manager(database).get_for_model(model_class)
             versioned_pk_queryset = Version.objects.using(database).filter(content_type=content_type).all()
             live_objs = model_class._default_manager.using(database).all()
-            if has_int_pk(model_class):
-                # We can do this as a fast database join!
-                live_objs = live_objs.exclude(
-                    pk__in=versioned_pk_queryset.values_list("object_id_int", flat=True)
-                )
-            else:
-                # This join has to be done as two separate queries.
-                live_objs = live_objs.exclude(
-                    pk__in=list(versioned_pk_queryset.values_list("object_id", flat=True).iterator())
-                )
+
+            # We can do this as a fast database join!
+            live_objs = live_objs.exclude(
+                pk__in=versioned_pk_queryset.values_list("object_id", flat=True)
+            )
+
             # Save all the versions.
             ids = list(live_objs.values_list(model_class._meta.pk.name, flat=True).order_by())
             total = len(ids)
--- src/reversion/models.py
+++ src/reversion/models.py
@@ -110,19 +110,6 @@ class Revision(models.Model):
         app_label = 'reversion'
 
 
-def has_int_pk(model):
-    """Tests whether the given model has an integer primary key."""
-    pk = model._meta.pk
-    return (
-        (
-            isinstance(pk, (models.IntegerField, models.AutoField)) and
-            not isinstance(pk, models.BigIntegerField)
-        ) or (
-            isinstance(pk, models.ForeignKey) and has_int_pk(remote_model(pk))
-        )
-    )
-
-
 class VersionQuerySet(models.QuerySet):
 
     def get_unique(self):
@@ -149,25 +136,19 @@ class Version(models.Model):
         help_text="The revision that contains this version.",
     )
 
-    object_id = models.TextField(
-        help_text="Primary key of the model under version control.",
-    )
-
-    object_id_int = models.IntegerField(
-        blank=True,
-        null=True,
-        db_index=True,
-        help_text="An indexed, integer version of the stored model's primary key, used for faster lookups.",
+    object_id = models.CharField(
+        max_length=191,
+        help_text="Primary key of the model under version control."
     )
 
     content_type = models.ForeignKey(
         ContentType,
         on_delete=models.CASCADE,
-        help_text="Content type of the model under version control.",
+        help_text="Content type of the model under version control.", db_index=False
     )
 
     # A link to the current instance, not the version stored in this Version!
-    object = GenericForeignKey()
+    object = GenericForeignKey(ct_field='content_type', fk_field='object_id')
 
     format = models.CharField(
         max_length=255,
@@ -234,3 +215,6 @@ class Version(models.Model):
 
     class Meta:
         app_label = 'reversion'
+        index_together = [
+            ["content_type", "object_id"],
+        ]
