724d91d0b797647023e9a896a1e4ea9003dd04b9
Michael Shannon <mshannon@urbn.com>
2016-05-16 17:35:24 -0400

    Change Object_Id to Char and Index
    - Data type corrections again, for cast in MySql & Postgresql

--- src/reversion/functions.py
+++ src/reversion/functions.py
@@ -9,8 +9,15 @@ class ReversionCast(Func):
     function = 'CAST'
     template = '%(function)s(%(expressions)s AS %(db_type)s)'
 
-    override_types = {
-        fields.AutoField: 'integer'
+    override_mysql_types = {
+        fields.AutoField: 'integer',
+        fields.CharField: 'char',
+        fields.PositiveIntegerField: 'integer',
+        fields.FloatField: 'signed',
+    }
+
+    override_postgresql_types = {
+        fields.AutoField: 'integer',
     }
 
     def __init__(self, expression, output_field):
@@ -23,12 +30,14 @@ class ReversionCast(Func):
 
     def as_mysql(self, compiler, connection):
         output_field_class = self._output_field.get_internal_type()
-        if output_field_class in self.override_types:
-            self.extra['db_type'] = self.override_types[output_field_class]
-        return self.as_sql(compiler, connection)
+        if output_field_class in self.override_mysql_types:
+            self.extra['db_type'] = self.override_mysql_types[output_field_class]
+        return self.as_sql(compiler, connection, template='%(function)s(%(expressions)s AS %(db_type)s) COLLATE ''utf8_unicode_ci''')
 
     def as_postgresql(self, compiler, connection):
         output_field_class = self._output_field.get_internal_type()
-        if output_field_class in self.override_types:
-            self.extra['db_type'] = self.override_types[output_field_class]
-        return self.as_sql(compiler, connection)
+        if output_field_class in self.override_postgresql_types:
+            self.extra['db_type'] = self.override_postgresql_types[output_field_class]
+        # CAST would be valid too, but the :: shortcut syntax is more readable.
+        return self.as_sql(compiler, connection, template='%(expressions)s::%(db_type)s')
+
