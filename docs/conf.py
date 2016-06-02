623ace8c10947ae6801092ecbc89316b3c3e9f2c
Dave Hall <dave@etianen.com>
2016-06-02 09:08:42 +0100

    Fixing docs on travis

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ from reversion import __version__
 extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = []
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -156,7 +156,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
