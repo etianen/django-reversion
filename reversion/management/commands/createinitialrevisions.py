3582ba4f64b56fa46db3cd648c1e726aca8ddda1
Dave Hall <dave@etianen.com>
2016-06-02 09:47:36 +0100

    Using self.stdout in createinitialrevisions command, rather than print

--- reversion/management/commands/createinitialrevisions.py
+++ reversion/management/commands/createinitialrevisions.py
@@ -1,12 +1,12 @@
 from __future__ import unicode_literals
-from collections import OrderedDict
+from collections import defaultdict
 from importlib import import_module
 from django.apps import apps
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.core.management.base import BaseCommand
 from django.core.management.base import CommandError
-from django.db import reset_queries
+from django.db import reset_queries, transaction
 from django.utils import translation
 from django.utils.encoding import force_text
 from reversion.revisions import default_revision_manager
@@ -18,20 +18,21 @@ def get_app(app_label):
 
 
 class Command(BaseCommand):
+
     help = "Creates initial revisions for a given app [and model]."
 
     def add_arguments(self, parser):
         parser.add_argument(
-            'args',
-            metavar='app_label',
-            nargs='*',
+            "args",
+            metavar="app_label",
+            nargs="*",
             help="Optional apps or app.Model list.",
         )
         parser.add_argument(
             "--comment",
             action="store",
             default="Initial version.",
-            help='Specify the comment to add to the revisions. Defaults to "Initial version.".')
+            help="Specify the comment to add to the revisions. Defaults to 'Initial version'.")
         parser.add_argument(
             "--batch-size",
             action="store",
@@ -41,69 +42,57 @@ class Command(BaseCommand):
         )
         parser.add_argument(
             "--database",
-            help='Nominates a database to create revisions in.',
+            help="Nominates a database to create revisions in.",
         )
 
+    @transaction.atomic()
     def handle(self, *app_labels, **options):
-
         # Activate project's default language
         translation.activate(settings.LANGUAGE_CODE)
-
+        # Parse options.
         comment = options["comment"]
         batch_size = options["batch_size"]
-        database = options.get('database')
-
+        database = options["database"]
         verbosity = int(options.get("verbosity", 1))
-        app_list = OrderedDict()
-        # if no apps given, use all installed.
+        # Look up apps.
+        app_models = defaultdict(set)
+        # If no apps given, use all installed.
         if len(app_labels) == 0:
             all_apps = [config.models_module for config in apps.get_app_configs()
                         if config.models_module is not None]
             for app in all_apps:
-                if app not in app_list:
-                    app_list[app] = []
                 for model_class in apps.get_models(app):
-                    if model_class not in app_list[app]:
-                        app_list[app].append(model_class)
+                    app_models[app].add(model_class)
         else:
             for label in app_labels:
-                try:
+                if "." in label:
+                    # This is an app.Model specifier.
                     app_label, model_label = label.split(".")
                     try:
                         app = get_app(app_label)
                     except ImproperlyConfigured:
                         raise CommandError("Unknown application: %s" % app_label)
-
                     model_class = apps.get_model(app_label, model_label)
                     if model_class is None:
                         raise CommandError("Unknown model: %s.%s" % (app_label, model_label))
-                    if app in app_list:
-                        if app_list[app] and model_class not in app_list[app]:
-                            app_list[app].append(model_class)
-                    else:
-                        app_list[app] = [model_class]
-                except ValueError:
+                    app_models[app].add(model_class)
+                else:
                     # This is just an app - no model qualifier.
                     app_label = label
                     try:
                         app = get_app(app_label)
-                        if app not in app_list:
-                            app_list[app] = []
                         for model_class in apps.get_models(app):
-                            if model_class not in app_list[app]:
-                                app_list[app].append(model_class)
+                            app_models[app].add(model_class)
                     except ImproperlyConfigured:
                         raise CommandError("Unknown application: %s" % app_label)
         # Create revisions.
-        for app, model_classes in app_list.items():
+        for app, model_classes in app_models.items():
             for model_class in model_classes:
                 self.create_initial_revisions(app, model_class, comment, batch_size, verbosity, database=database)
-
         # Go back to default language
         translation.deactivate()
 
     def create_initial_revisions(self, app, model_class, comment, batch_size, verbosity=2, database=None, **kwargs):
-        """Creates the set of initial revisions for the given model."""
         # Import the relevant admin module.
         try:
             import_module("%s.admin" % app.__name__.rsplit(".", 1)[0])
@@ -112,7 +101,9 @@ class Command(BaseCommand):
         # Check all models for empty revisions.
         if default_revision_manager.is_registered(model_class):
             if verbosity >= 2:
-                print("Creating initial revision(s) for model %s ..." % (force_text(model_class._meta.verbose_name)))
+                self.stdout.write("Creating initial revision(s) for model %s ..." % (
+                    force_text(model_class._meta.verbose_name)
+                ))
             created_count = 0
             content_type = default_revision_manager._get_content_type(model_class, db=database)
             live_objs = model_class._base_manager.using(database).exclude(
@@ -134,19 +125,21 @@ class Command(BaseCommand):
                             db=database,
                         )
                     except:
-                        print("ERROR: Could not save initial version for %s %s." % (model_class.__name__, obj.pk))
+                        self.stdout.write("ERROR: Could not save initial version for %s %s." % (
+                            model_class.__name__,
+                            obj.pk,
+                        ))
                         raise
                     created_count += 1
                 reset_queries()
                 if verbosity >= 2:
-                    print("Created %s of %s." % (created_count, total))
-
+                    self.stdout.write("Created %s of %s." % (created_count, total))
             # Print out a message, if feeling verbose.
             if verbosity >= 2:
-                print("Created %s initial revision(s) for model %s." % (
+                self.stdout.write("Created %s initial revision(s) for model %s." % (
                     created_count,
                     force_text(model_class._meta.verbose_name),
                 ))
         else:
             if verbosity >= 2:
-                print("Model %s is not registered." % (force_text(model_class._meta.verbose_name)))
+                self.stdout.write("Model %s is not registered." % (force_text(model_class._meta.verbose_name)))
