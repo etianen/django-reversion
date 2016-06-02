263f9c9b90704c8ed6809bff63c0ff12782d224c
Dave Hall <dave@etianen.com>
2016-06-02 07:19:16 +0100

    Removing unused signal.

--- reversion/signals.py
+++ reversion/signals.py
@@ -1,21 +1,5 @@
 from django.dispatch.dispatcher import Signal
 
 
-# Signal sent just before a new Revision object is saved to the database.
 pre_revision_commit = Signal(providing_args=["instances", "revision", "versions"])
-
-
-# Signal sent just after a new Revision object is saved to the database.
 post_revision_commit = Signal(providing_args=["instances", "revision", "versions"])
-
-
-# Signal sent just
-post_revision_context_end = Signal(providing_args=[
-    "objects",
-    "serialized_objects",
-    "ignore_duplicates",
-    "user",
-    "comment",
-    "meta",
-    "db",
-])
