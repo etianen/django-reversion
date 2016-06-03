30d48c7d1223212554ee22cb0e66c4ab0fd924ce
Dave Hall <dave@etianen.com>
2016-06-03 17:14:45 +0100

    Cleaning out existing tests, and switching to a multi-db test setup

--- tests/test_project/urls.py
+++ tests/test_project/urls.py
@@ -1,16 +1,11 @@
-from django.conf.urls import include, url
+from django.conf.urls import url, include
 from django.contrib import admin
-from test_app import views
 
 
 urlpatterns = [
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r"^admin/", admin.site.urls),
 
-    url(r"^success-base/$", views.SaveRevisionViewBase.as_view()),
-    url(r"^success/$", views.SaveRevisionView.as_view()),
-
-    url(r"^error-base/$", views.ErrorRevisionViewBase.as_view()),
-    url(r"^error/$", views.ErrorRevisionView.as_view()),
+    url(r"^", include("test_app.urls", namespace="test")),
 
 ]
--- tests/test_project/wsgi.py
+++ tests/test_project/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for tests project.
+WSGI config for test_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
