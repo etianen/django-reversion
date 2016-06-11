7a85443a91ad8dc512fdb4ba7c28be6adffdd959
Dave Hall <dave@etianen.com>
2016-06-11 04:18:30 +0100

    Testing revert

--- tests/test_app/tests/test_admin.py
+++ tests/test_app/tests/test_admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.shortcuts import resolve_url
 import reversion
 from reversion.admin import VersionAdmin
+from reversion.models import Version
 from test_app.models import TestModel, TestModelParent
 from test_app.tests.base import LoginTestBase
 
@@ -45,30 +46,49 @@ class AdminUpdateViewTest(AdminTestBase):
         obj = TestModelParent.objects.create()
         self.client.post(resolve_url("admin:test_app_testmodelparent_change", obj.pk), {
             "name": "v2",
-            "parent_name": "parent_v2",
+            "parent_name": "parent v2",
         })
         self.assertSingleRevision((obj, obj.testmodel_ptr), user=self.user, comment="Changed name and parent_name.")
 
 
 class AdminRevisionViewTest(AdminTestBase):
 
+    def setUp(self):
+        super(AdminRevisionViewTest, self).setUp()
+        with reversion.create_revision():
+            self.obj = TestModelParent.objects.create()
+        with reversion.create_revision():
+            self.obj.name = "v2"
+            self.obj.parent_name = "parent v2"
+            self.obj.save()
+
     def testRevisionView(self):
-        with reversion.create_revision():
-            obj = TestModelParent.objects.create()
-        with reversion.create_revision():
-            obj.name = "v2"
-            obj.parent_name = "parent v2"
         response = self.client.get(resolve_url(
             "admin:test_app_testmodelparent_revision",
-            obj.pk,
-            reversion.get_for_object(obj)[1].pk,
+            self.obj.pk,
+            Version.objects.get_for_object(self.obj)[1].pk,
         ))
         self.assertContains(response, 'value="v1"')
         self.assertContains(response, 'value="parent v1"')
+
+    def testRevisionViewOldRevision(self):
         response = self.client.get(resolve_url(
             "admin:test_app_testmodelparent_revision",
-            obj.pk,
-            reversion.get_for_object(obj)[-].pk,
+            self.obj.pk,
+            Version.objects.get_for_object(self.obj)[0].pk,
         ))
         self.assertContains(response, 'value="v2"')
         self.assertContains(response, 'value="parent v2"')
+
+    def testRevisionViewRevert(self):
+        self.client.post(resolve_url(
+            "admin:test_app_testmodelparent_revision",
+            self.obj.pk,
+            Version.objects.get_for_object(self.obj)[1].pk,
+        ), {
+            "name": "v1",
+            "parent_name": "parent v1",
+        })
+        self.obj.refresh_from_db()
+        self.assertEqual(self.obj.name, "v1")
+        self.assertEqual(self.obj.parent_name, "parent v1")
