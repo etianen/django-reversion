a58158eab649e6a7b3c49f8331db4f4c0625c1a7
b.khasanov <afti@yandex.ru>
2016-08-25 23:47:41 +0300

    add `is_authenticated` compatibility django 1.10+

--- reversion/compat.py
+++ reversion/compat.py
@@ -1,3 +1,6 @@
+import django
+
+
 def remote_field(field):
     # remote_field is new in Django 1.9
     return field.remote_field if hasattr(field, 'remote_field') else field.rel
@@ -6,3 +9,9 @@ def remote_field(field):
 def remote_model(field):
     # remote_field is new in Django 1.9
     return field.remote_field.model if hasattr(field, 'remote_field') else field.rel.to
+
+
+def is_authenticated(user):
+    if django.VERSION < (1, 10):
+        return user.is_authenticated()
+    return user.is_authenticated
