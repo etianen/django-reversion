c768c8bd6ec86c7a4aab34a717372685dae59b85
Dave Hall <dave@etianen.com>
2018-04-30 10:41:24 +0100

    Updating setup.py to list support for python 3.6

--- setup.py
+++ setup.py
@@ -38,8 +38,8 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         "Framework :: Django",
     ]
 )
