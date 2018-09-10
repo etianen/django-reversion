36bc1b7e0fce342e284b33441ae168c72fe31cfd
Michael Käufl <michael-k@users.noreply.github.com>
2018-09-10 18:43:06 +0200

    Add support for Python 3.7

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         "Framework :: Django",
     ]
 )
