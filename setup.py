d4c0ffe1f233d9510025bd6c204390f7c56ec03c
Dave Hall <dave@etianen.com>
2019-12-02 17:59:04 +0000

    Updating trove classifiers

--- setup.py
+++ setup.py
@@ -37,9 +37,9 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         "Framework :: Django",
     ]
 )
