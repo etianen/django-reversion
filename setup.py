090c00ac5f417fb90f9024c4b601d9ec908b791c
Léo S <leo@naeka.fr>
2022-08-10 10:09:29 +0200

    Upgrade minimal dependencies to Django 3.2 Add Django 4.1 to test matrix

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         "reversion": ["locale/*/LC_MESSAGES/django.*", "templates/reversion/*.html"]},
     cmdclass=cmdclass,
     install_requires=[
-        "django>=2.0",
+        "django>=3.2",
     ],
     python_requires='>=3.7',
     classifiers=[
